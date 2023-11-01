685546490eeaaa216c8444d475e41153210f3eca
Long Bảo <114165590+LongBaoCoder2@users.noreply.github.com>
2023-11-01 16:07:58 +0700

    Fix bug

--- src/main.cpp
+++ src/main.cpp
@@ -25,10 +25,12 @@ private:
     void OnAbout(wxCommandEvent &event);
     wxDECLARE_EVENT_TABLE();
 };
+
 enum
 {
     ID_Hello = 1
 };
+
 wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
     EVT_MENU(ID_Hello, MyFrame::OnHello)
         EVT_MENU(wxID_EXIT, MyFrame::OnExit)
