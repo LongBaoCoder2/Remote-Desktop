8d3ffab37ebbcdde5a4f6ea4e4d87527af602e59
Nguyen Cao Cuong <nguyencaocuong@Nguyens-MacBook-Pro-2.local>
2023-11-02 11:15:21 +0700

    Adding all essentials header

---
+++ src/networks/net_common.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <memory>
+#include <thread>
+#include <mutex>
+#include <deque>
+#include <optional>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <chrono>
+#include <cstdint>
+#define ASIO_STANDALONE
+#include <asio.hpp>
+#include <asio/ts/buffer.hpp>
+#include <asio/ts/internet.hpp>
---
+++ src/networks/net_message.h
@@ -0,0 +1,60 @@
+#pragma once
+#include "net_common.h"
+#include "net_session.h"
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <cstdint>
+
+namespace net
+{
+
+    template <typename T>
+    struct message_header;
+
+    template <typename T>
+    struct message;
+
+    template <typename T>
+    std::ostream &operator<<(std::ostream &os, const message<T> &msg);
+
+    template <typename T>
+    struct owned_message;
+
+    // Message Header
+    template <typename T>
+    struct message_header
+    {
+        T id{};
+        uint32_t size = 0;
+    };
+
+    // Message Body
+    template <typename T>
+    struct message
+    {
+        message_header<T> header{};
+        std::vector<uint8_t> body;
+
+        size_t size() const;
+
+        friend std::ostream &operator<<(std::ostream &os, const message<T> &msg);
+
+        template <typename DataType>
+        friend message<T> &operator<<(message<T> &msg, const DataType &data);
+
+        template <typename DataType>
+        friend message<T> &operator>>(message<T> &msg, DataType &data);
+    };
+
+    // Owned Message
+    template <typename T>
+    struct owned_message
+    {
+        std::shared_ptr<connection<T>> remote = nullptr;
+        message<T> msg;
+
+        friend std::ostream &operator<<(std::ostream &os, const owned_message<T> &msg);
+    };
+
+}
