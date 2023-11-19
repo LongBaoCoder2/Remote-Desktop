73e4258aa8eebfe82c98653f58b6ed18a56b1899
Long Bảo <114165590+LongBaoCoder2@users.noreply.github.com>
2023-11-19 19:11:37 +0700

    Remove unessential files

--- src/windows/MainWindow/NavigationBar/NavigationBar.cpp
+++ src/windows/MainWindow/NavigationBar/NavigationBar.cpp
@@ -20,28 +20,28 @@ NavigationBar::NavigationBar(wxWindow* parent,
 
     BtnNav = new NavigationButtons(this);
 
-    // Create user icon and username information
-    UserInfoPanel = new wxPanel(this, wxID_ANY);
-    UserInfoPanel->SetWindowStyle(wxALIGN_BOTTOM);
-    UserSizer = new wxBoxSizer(wxVERTICAL);
-    wxImage iconImage("./assets/user_icon.png", wxBITMAP_TYPE_PNG);
+    // // Create user icon and username information
+    // UserInfoPanel = new wxPanel(this, wxID_ANY);
+    // UserInfoPanel->SetWindowStyle(wxALIGN_BOTTOM);
+    // UserSizer = new wxBoxSizer(wxVERTICAL);
+    // wxImage iconImage("./assets/user_icon.png", wxBITMAP_TYPE_PNG);
 
-    // Check image that is available
-    if (iconImage.IsOk())
-    {
-        wxBitmap iconBitmap(iconImage.Rescale(64, 64));
-        auto userBitmap = new wxStaticBitmap(UserInfoPanel, wxID_ANY, iconBitmap);
-        UserSizer->Add(userBitmap, 0, wxALL, FromDIP(5));
-    }
+    // // Check image that is available
+    // if (iconImage.IsOk())
+    // {
+    //     wxBitmap iconBitmap(iconImage.Rescale(64, 64));
+    //     auto userBitmap = new wxStaticBitmap(UserInfoPanel, wxID_ANY, iconBitmap);
+    //     UserSizer->Add(userBitmap, 0, wxALL, FromDIP(5));
+    // }
 
-    auto userText = new wxStaticText(UserInfoPanel, wxID_ANY, "Username");
-    UserSizer->Add(userText, 0, wxALIGN_CENTER | wxALL, FromDIP(5));
-    UserInfoPanel->SetSizerAndFit(UserSizer);
+    // auto userText = new wxStaticText(UserInfoPanel, wxID_ANY, "Username");
+    // UserSizer->Add(userText, 0, wxALIGN_CENTER | wxALL, FromDIP(5));
+    // UserInfoPanel->SetSizerAndFit(UserSizer);
 
     // Add elements to the NavSizer
     NavSizer->Add(TitleText, 0, wxALIGN_CENTER | wxTOP | wxBOTTOM, FromDIP(25));
-    NavSizer->Add(BtnNav, 0, wxEXPAND | wxTOP | wxBOTTOM, FromDIP(40));
-    NavSizer->Add(UserInfoPanel, 0, wxALIGN_CENTER | wxTOP, FromDIP(60));
+    NavSizer->Add(BtnNav, 0, wxEXPAND | wxTOP | wxBOTTOM, FromDIP(50));
+    // NavSizer->Add(UserInfoPanel, 0, wxALIGN_CENTER | wxTOP, FromDIP(60));
 
     // Set the background color for the NavigationBar
     this->SetBackgroundColour(CONFIG_UI::PRIMARY_DARK_COLOR);
